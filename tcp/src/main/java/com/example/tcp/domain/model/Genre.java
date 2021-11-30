69cbb2e1b4f1ce5d3bccfe735e1c4e83f5a717d4
toni carpa <tcp.new.id@gmail>
2021-11-30 20:25:57 +0100

    step2

--- tcp/src/main/java/com/example/tcp/domain/model/Genre.java
+++ tcp/src/main/java/com/example/tcp/domain/model/Genre.java
@@ -1,4 +1,22 @@
 package com.example.tcp.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+import java.util.UUID;
+
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public UUID genreid;
+
+    public String label;
+
+    @ManyToMany(mappedBy = "genres")
+    @JsonIgnoreProperties("genres")
+    public Set<Anime> animes;
 }
---
+++ tcp/src/main/java/com/example/tcp/domain/model/projection/ProFile.java
@@ -0,0 +1,8 @@
+package com.example.tcp.domain.model.projection;
+
+import java.util.UUID;
+
+public interface ProFile {
+    public UUID getFileid();
+    String getContenttype();
+}
